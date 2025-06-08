f46afa7c2bb5ab09c69b478cf0fd6adf6ffd06e9
Timothé Couturier <99717711+TimCout@users.noreply.github.com>
2025-06-08 09:23:18 +0200

    layout modification

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,6 +48,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="CreateMatch"
+        options={{
+          title: 'Create a Match',
+          //tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="Logout"
         options={{
           title: 'Logout',

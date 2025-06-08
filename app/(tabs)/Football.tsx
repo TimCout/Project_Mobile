a2cccc66069a3b94daf18af3beec049a924b7f9d
Timothé Couturier <99717711+TimCout@users.noreply.github.com>
2025-06-08 09:25:24 +0200

    Creation of football page

--- app/(tabs)/Football.tsx
+++ app/(tabs)/Football.tsx
@@ -5,38 +5,59 @@ import {
   FlatList,
   StyleSheet,
   TouchableOpacity,
-  Button,
 } from "react-native";
 import moment from "moment";
-import { string, z } from "zod";
+import { useState } from "react";
+import { z } from "zod";
+import { useAuth } from "@/lib/auth";
+import ProtectedLayout from "@/components/ProtectedLayout";
 
 const matchSchema = z.object({
   id: z.number(),
   date: z.string(),
   place: z.string(),
   level: z.string(),
-  registeredPlayer: z.string(),
-  neededPlayer: z.string(),
+  registeredPlayer: z.number(),
+  neededPlayer: z.number(),
 });
 type Match = z.infer<typeof matchSchema>;
 
 export default function Football() {
   const queryClient = useQueryClient();
+  const { user } = useAuth()
+
   const { data: matches, isLoading } = useQuery({
     queryKey: ["matches"],
     queryFn: async () => {
-      const res = await fetch("http://localhost:3000/api/matches");
+      const res = await fetch("http://localhost:3000/api/matches", { credentials: 'include'});
       const data = await res.json();
-      return data as Match[];
+      return z.array(matchSchema).parse(data);
+    },
+  });
+  
+  const { data: participation } = useQuery({
+    queryKey: ["hasJoined"],
+    queryFn: async() => {
+      const res = await fetch(`http://localhost:3000/api/matches/join`, { credentials: 'include'})
+      return (await res.json()) as { id: number, joined: boolean}[];
+    }
+  })
+
+  const { mutate: joinMatch } = useMutation({
+    mutationFn: async (id: number) => {
+      await fetch(`http://localhost:3000/api/matches/join/${id}`, { method: "POST", credentials: "include" });
+    },
+    onSuccess: (_, id) => {
+      queryClient.invalidateQueries();
     },
   });
 
-  const { mutate: join } = useMutation({
+  const { mutate: leaveMatch } = useMutation({
     mutationFn: async (id: number) => {
-      await fetch("http://localhost:3000/api/matches/join/" + id);
+      await fetch(`http://localhost:3000/api/matches/leave/${id}`, { method: "POST", credentials: "include" });
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["matches"] });
+    onSuccess: (_, id) => {
+      queryClient.invalidateQueries();
     },
   });
 
@@ -47,37 +68,47 @@ export default function Football() {
   if (isLoading) return <Text>Loading...</Text>;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>THEY WANT YOU</Text>
+    <ProtectedLayout>
+      <View style={styles.container}>
+        <Text style={styles.title}>THEY WANT YOU, {user?.username}</Text>
 
-      {(!availableMatches || availableMatches.length === 0) && (
-        <Text style={styles.noMatch}>
-          Aucun match disponible pour le moment
-        </Text>
-      )}
-
-      <FlatList
-        data={availableMatches}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Text style={styles.matchText}>
-              <Text style={styles.bold}>
-                {moment(item.date).format("LLLL")}
-              </Text>{" "}
-              - <Text style={styles.bold}>{item.place}</Text> -{" "}
-              <Text style={styles.italic}>Level : {item.level}</Text> -{" "}
-              <Text style={styles.players}>
-                {item.registeredPlayer}/{item.neededPlayer} players
-              </Text>
-            </Text>
-            <TouchableOpacity style={styles.button}>
-              <Button title="I'm in" onPress={() => join(item.id)} />
-            </TouchableOpacity>
-          </View>
+        {(!availableMatches || availableMatches.length === 0) && (
+          <Text style={styles.noMatch}>Aucun match disponible pour le moment</Text>
         )}
-      />
-    </View>
+
+        <FlatList
+          data={availableMatches}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => {
+            const joined = (participation?.filter(p => p.id === item.id && p.joined).length ?? 0) > 0
+            return (
+              <View style={styles.card}>
+                <Text style={styles.matchText}>
+                  <Text style={styles.bold}>
+                    {moment(item.date).format("LLLL")}
+                  </Text>{" "}
+                  - <Text style={styles.bold}>{item.place}</Text> -{" "}
+                  <Text style={styles.italic}>Level : {item.level}</Text> -{" "}
+                  <Text style={styles.players}>
+                    {item.registeredPlayer}/{item.neededPlayer} players
+                  </Text>
+                </Text>
+
+                <TouchableOpacity
+                  style={[
+                    styles.button,
+                    { backgroundColor: joined ? "#b91c1c" : "#0ea5e9" },
+                  ]}
+                  onPress={() => (joined ? leaveMatch(item.id) : joinMatch(item.id))}
+                >
+                  <Text style={styles.buttonText}>{joined ? "I'm out" : "I'm in"}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        />
+      </View>
+    </ProtectedLayout>
   );
 }
 
@@ -126,7 +157,6 @@ const styles = StyleSheet.create({
     color: "#f97316",
   },
   button: {
-    backgroundColor: "#0ea5e9",
     paddingVertical: 10,
     paddingHorizontal: 16,
     borderRadius: 8,
